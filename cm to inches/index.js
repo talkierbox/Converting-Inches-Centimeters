670b5349817267b20919ed3188b799143f9e379d
Hershraj N <60162641+talkierbox@users.noreply.github.com>
2020-01-21 17:43:57 -0800

    First Commit

---
+++ cm to inches/index.js
@@ -0,0 +1,9 @@
+let convertArray = [158]
+
+
+// Converting Centimeters to Inches
+
+
+for (var i = 0; i < convertArray.length; i++){
+    console.log(`Question #${i + 1}:  INCHES: ${convertArray[i] * 0.394} [CM: ${convertArray[i]}]`)
+}
---
+++ inches to cm/index.js
@@ -0,0 +1,7 @@
+let convertArray = []
+
+// Converting Inches to Centimeters
+
+for (var i = 0; i < convertArray.length; i++){
+    console.log(`Question #${i + 1}:  CM: ${convertArray[i] / 0.394} [INCHES: ${convertArray[i]}]`)
+}
